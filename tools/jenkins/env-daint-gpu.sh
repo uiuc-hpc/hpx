cd202a02423b099566f23a62b6f87763612923f5
Mikael Simberg <simberg@cscs.ch>
2020-06-20 17:28:52 +0200

    temp

--- tools/jenkins/env-daint-gpu.sh
+++ tools/jenkins/env-daint-gpu.sh
@@ -3,5 +3,6 @@ export CRAYPE_LINK_TYPE=dynamic
 module load daint-gpu
 module load CMake
 module load cudatoolkit
-module load Boost
-module load hwloc/.2.0.3
+
+export BOOST_ROOT=/apps/daint/UES/simbergm/local/boost-1.69.0-gcc-8.3.0-c++17-release
+export HWLOC_ROOT=/apps/daint/UES/simbergm/local/hwloc-2.0.3-gcc-8.3.0
