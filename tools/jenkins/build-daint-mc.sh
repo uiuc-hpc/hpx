91dc2c9222bce3ba51e3904d437efde8688ae8d6
Mikael Simberg <simberg@cscs.ch>
2020-06-23 14:05:27 +0200

    temp

--- tools/jenkins/build-daint-mc.sh
+++ tools/jenkins/build-daint-mc.sh
@@ -12,17 +12,16 @@ set -eux
 source ../tools/jenkins/env-daint-mc.sh
 
 srcdir=$(pwd)
-builddir=/dev/shm/build
+builddir=/dev/shm/hpx/build
 
 mkdir -p $builddir
 cd $builddir
 rm -rf *
 cmake $srcdir \
-    -GNinja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DHPX_PROGRAM_OPTIONS_WITH_BOOST_PROGRAM_OPTIONS_COMPATIBILITY=OFF \
     -DHPX_WITH_MALLOC=system
 
-ninja -j10 all tests
+make -j10 all tests
 
 ctest --output-on-failure
--- tools/jenkins/env-daint-mc.sh
+++ tools/jenkins/env-daint-mc.sh
@@ -7,16 +7,36 @@
 #
 #export PATH=/apps/daint/UES/simbergm/spack/opt/spack/cray-cnl7-haswell/gcc-8.3.0/ninja-1.10.0-dcy5yzzldhss6wycy2ejjwj7o75dfddz/bin:$PATH
 
+#export CRAYPE_LINK_TYPE=dynamic
+#
+#module load daint-gpu
+#module load CMake
+#module load cudatoolkit
+#
+#export LOCAL_INSTALL=/apps/daint/UES/simbergm/local
+#export BOOST_ROOT=$LOCAL_INSTALL/boost-1.69.0-gcc-8.3.0-c++17-release
+#export HWLOC_ROOT=$LOCAL_INSTALL/hwloc-2.0.3-gcc-8.3.0
+#export CXX=$(which CC)
+#export CC=$(which cc)
+#
+#export PATH=/apps/daint/UES/simbergm/spack/opt/spack/cray-cnl7-haswell/gcc-8.3.0/ninja-1.10.0-dcy5yzzldhss6wycy2ejjwj7o75dfddz/bin:$PATH
 export CRAYPE_LINK_TYPE=dynamic
 
-module load daint-gpu
-module load CMake
-module load cudatoolkit
+export LOCAL_ROOT="/apps/daint/UES/simbergm/local"
+export GCC_VER="10.1.0"
+export CXX_STD="17"
+export BOOST_VER="1.73.0"
+export TCMALLOC_VER="2.7"
+export HWLOC_VER="2.2.0"
+export GCC_ROOT="${LOCAL_ROOT}/gcc-${GCC_VER}"
+export BOOST_ROOT="${LOCAL_ROOT}/boost-${BOOST_VER}-gcc-${GCC_VER}-c++${CXX_STD}-debug"
+export HWLOC_ROOT="${LOCAL_ROOT}/hwloc-${HWLOC_VER}-gcc-${GCC_VER}"
+export TCMALLOC_ROOT="${LOCAL_ROOT}/gperftools-${TCMALLOC_VER}-gcc-${GCC_VER}"
+export CFLAGS=""
+export CXXFLAGS="-fdiagnostics-color=always -nostdinc++ -I${GCC_ROOT}/include/c++/${GCC_VER} -I${GCC_ROOT}/include/c++/${GCC_VER}/x86_64-unknown-linux-gnu -I${GCC_ROOT}/include/c++/${GCC_VER}/x86_64-pc-linux-gnu -L${GCC_ROOT}/lib64 -Wl,-rpath,${GCC_ROOT}/lib64"
+export LDFLAGS="-L${GCC_ROOT}/lib64"
+export LDFLAGS="-L${GCC_ROOT}/lib64"
+export CXX=${GCC_ROOT}/bin/g++
+export CC=${GCC_ROOT}/bin/gcc
 
-export LOCAL_INSTALL=/apps/daint/UES/simbergm/local
-export BOOST_ROOT=$LOCAL_INSTALL/boost-1.69.0-gcc-8.3.0-c++17-release
-export HWLOC_ROOT=$LOCAL_INSTALL/hwloc-2.0.3-gcc-8.3.0
-export CXX=$(which CC)
-export CC=$(which cc)
-
-export PATH=/apps/daint/UES/simbergm/spack/opt/spack/cray-cnl7-haswell/gcc-8.3.0/ninja-1.10.0-dcy5yzzldhss6wycy2ejjwj7o75dfddz/bin:$PATH
+module load daint-mc
