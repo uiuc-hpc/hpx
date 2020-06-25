9171e56b363549194f1d4761550bd0a983994370
Mikael Simberg <simberg@cscs.ch>
2020-06-25 12:15:54 +0200

    temp

--- tools/jenkins/env-daint-mc.sh
+++ tools/jenkins/env-daint-mc.sh
@@ -1,16 +1,17 @@
-source /apps/daint/SSL/HPX/spack/share/spack/setup-env.sh
+export SPACK_ROOT=/apps/daint/SSL/HPX/spack
+source $SPACK_ROOT/share/spack/setup-env.sh
 
 export CRAYPE_LINK_TYPE=dynamic
 
-export LOCAL_ROOT="/apps/daint/UES/simbergm/local"
+export APPS_ROOT="/apps/daint/UES/simbergm/local"
 export GCC_VER="10.1.0"
 export CXX_STD="17"
 export BOOST_VER="1.73.0"
 export TCMALLOC_VER="2.7"
 export HWLOC_VER="2.2.0"
-export GCC_ROOT="${LOCAL_ROOT}/gcc-${GCC_VER}"
-export BOOST_ROOT="${LOCAL_ROOT}/boost-${BOOST_VER}-gcc-${GCC_VER}-c++${CXX_STD}-debug"
-export HWLOC_ROOT="${LOCAL_ROOT}/hwloc-${HWLOC_VER}-gcc-${GCC_VER}"
+export GCC_ROOT="${APPS_ROOT}/gcc-${GCC_VER}"
+export BOOST_ROOT="${APPS_ROOT}/boost-${BOOST_VER}-gcc-${GCC_VER}-c++${CXX_STD}-debug"
+export HWLOC_ROOT="${APPS_ROOT}/hwloc-${HWLOC_VER}-gcc-${GCC_VER}"
 export CXXFLAGS="-nostdinc++ -I${GCC_ROOT}/include/c++/${GCC_VER} -I${GCC_ROOT}/include/c++/${GCC_VER}/x86_64-unknown-linux-gnu -I${GCC_ROOT}/include/c++/${GCC_VER}/x86_64-pc-linux-gnu -L${GCC_ROOT}/lib64 -Wl,-rpath,${GCC_ROOT}/lib64"
 export LDFLAGS="-L${GCC_ROOT}/lib64"
 export CXX=${GCC_ROOT}/bin/g++
