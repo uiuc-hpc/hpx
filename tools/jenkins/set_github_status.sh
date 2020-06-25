b49133be87a28dd31241ecebbb4a77e7601b2492
Mikael Simberg <simberg@cscs.ch>
2020-06-25 15:52:01 +0200

    temp

---
+++ tools/jenkins/set_github_status.sh
@@ -0,0 +1,17 @@
+#/usr/bin/env bash
+
+set -eu
+
+github_token=${1}
+commit_repo=${2}
+commit_sha=${3}
+commit_status=${4}
+configuration_name=${5}
+build_id=${6}
+
+curl --verbose \
+    --request POST \
+    --url "https://api.github.com/repos/${commit_repo}/statuses/${commit_sha}" \
+    --header 'Content-Type: application/json' \
+    --header "authorization: Bearer ${github_token}" \
+    --data "{ \"state\": \"${commit_status}\", \"target_url\": \"https://cdash.cscs.ch/buildSummary.php?buildid=${build_id}\", \"description\": \"Jenkins\", \"context\": \"continuous-integration/jenkins/branch/${configuration_name}\" }"
