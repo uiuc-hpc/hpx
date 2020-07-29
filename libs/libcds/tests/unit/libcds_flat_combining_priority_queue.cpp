490224e18054d1252eeb29b1386cd500050cb919
Weile Wei <lokwei9@gmail.com>
2020-07-28 22:17:53 -0500

    fix libcds fc-pq example and remove hpx threading in it

--- libs/libcds/tests/unit/libcds_flat_combining_priority_queue.cpp
+++ libs/libcds/tests/unit/libcds_flat_combining_priority_queue.cpp
@@ -5,12 +5,9 @@
 //  (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/assert.hpp>
 #include <hpx/hpx_init.hpp>
-#include <hpx/include/threads.hpp>
 
 #include <cds/container/fcpriority_queue.h>
-#include <cds/gc/hp.h>    // for cds::HP (Hazard Pointer) SMR
 #include <cds/init.h>     // for cds::Initialize and cds::Terminate
 
 #include <algorithm>
@@ -20,57 +17,43 @@
 #include <iterator>
 #include <random>
 #include <vector>
+#include <cassert>
 
-template <class PQueue>
-void run(PQueue& pq)
-{
+template<class PQueue>
+void run(PQueue &pq) {
     // generate a list of random numbers
     std::size_t num_elem = 10000;
-    std::vector<std::size_t> nums(num_elem);
+    std::vector <std::size_t> nums(num_elem);
     std::generate(nums.begin(), nums.end(), std::rand);
 
-    // launch num_elem of hpx threads, each of which puts a random number
+    // launch num_elem of threads, each of which puts a random number
     // into fc priority queue
-    std::vector<hpx::future<void>> futures;
-    for (auto i : nums)
-    {
-        futures.emplace_back(hpx::async([&, i]() {
-            // Use hazard pointer threading explicitly
-            cds::gc::hp::smr::attach_thread();
-            pq.push(i);
-        }));
+    for (auto i : nums) {
+        pq.push(i);
     }
 
     // sort the list to expected order
     std::sort(nums.begin(), nums.end());
 
-    // cds::gc::hp::detach_thread() will be automatically called
-    // upon each hazard pointer thread get destructed
-    hpx::wait_all(futures);
-
     // valid if fc priority queue sorts elements in order
-    for (auto i : nums)
-    {
+    for (auto i : nums) {
         std::size_t val;
         pq.pop(val);
-        HPX_ASSERT(i == val);
+        assert(i == val);
     }
 }
 
-int hpx_main(int, char**)
-{
+int hpx_main(int, char **) {
     // Initialize libcds
     cds::Initialize();
 
     {
-        cds::gc::HP hpGC;
-        // Initialize Hazard Pointer singleton
         cds::threading::Manager::attachThread();
 
         // Create flat-combining priority queue object
-        using fc_pqueue_type = cds::container::FCPriorityQueue<std::size_t,
-            std::priority_queue<std::size_t, std::deque<std::size_t>,
-                std::greater<std::size_t>>>;
+        using fc_pqueue_type = cds::container::FCPriorityQueue <std::size_t,
+        std::priority_queue<std::size_t, std::deque < std::size_t>,
+        std::greater<std::size_t>>>;
 
         fc_pqueue_type pq;
 
@@ -85,7 +68,6 @@ int hpx_main(int, char**)
     return hpx::finalize();
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     return hpx::init(argc, argv);
 }
