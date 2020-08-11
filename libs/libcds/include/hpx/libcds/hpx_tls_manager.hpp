3249ba45ce0c3bffe8af6f5e3555aede9809cb2e
Weile Wei <lokwei9@gmail.com>
2020-08-11 16:07:17 -0500

    WIP try fetch libcds master branch and use hpxtls manager in libcds module

---
+++ libs/libcds/include/hpx/libcds/hpx_tls_manager.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2020 Weile Wei
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef LIBCDS_HPX_TLS_MANAGER
+#define LIBCDS_HPX_TLS_MANAGER
+
+#include <cds/gc/details/hp_common.h>
+
+//@cond
+namespace cds { namespace gc { namespace hp { namespace common {
+
+    class HPXTLSManager
+    {
+    public:
+        static CDS_EXPORT_API thread_data* getTLS();
+        static CDS_EXPORT_API void setTLS(thread_data*);
+    };
+
+}}}}    // namespace cds::gc::hp::details
+
+#endif    // #ifndef LIBCDS_HPX_TLS_MANAGER
---
+++ libs/libcds/src/hpx_tls_manager.cpp
@@ -0,0 +1,30 @@
+//  (C) Copyright 2020 Weile Wei
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/modules/threading.hpp>
+#include <hpx/threading_base/thread_data.hpp>
+#include <hpx/libcds/hpx_manager.hpp>
+
+#include <cds/gc/details/hp_common.h>
+
+namespace cds { namespace gc { namespace hp { namespace details {
+
+    /*static*/ CDS_EXPORT_API thread_data* HPXTLSManager::getTLS()
+    {
+        auto thread_id = hpx::threads::get_self_id();
+        std::size_t hpx_tls_data =
+            hpx::threads::get_libcds_hazard_pointer_data(thread_id);
+        return reinterpret_cast<thread_data*>(hpx_tls_data);
+    }
+
+    /*static*/CDS_EXPORT_API void HPXTLSManager::setTLS(thread_data* new_tls)
+    {
+        auto thread_id = hpx::threads::get_self_id();
+        size_t hp_tls_data = reinterpret_cast<std::size_t>(new_tls);
+        hpx::threads::set_libcds_hazard_pointer_data(thread_id, hp_tls_data);
+    }
+
+}}}}    // namespace cds::gc::hp::details
