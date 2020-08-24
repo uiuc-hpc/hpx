71744912f6300fd745d0322aabf3a4efec5ffa5b
Weile Wei <lokwei9@gmail.com>
2020-08-24 13:39:08 -0500

    move all map tests into one single file

--- libs/libcds/tests/unit/libcds_map_hazard_pointer_hpxthread.cpp
+++ libs/libcds/tests/unit/libcds_map_hazard_pointer_hpxthread.cpp
@@ -12,6 +12,10 @@
 #include <hpx/modules/testing.hpp>
 
 #include <cds/container/feldman_hashmap_hp.h>
+#include <cds/container/michael_kvlist_hp.h>
+#include <cds/container/michael_list_hp.h>
+#include <cds/container/michael_map.h>
+#include <cds/container/split_list_map.h>
 #include <cds/init.h>    // for cds::Initialize and cds::Terminate
 
 #include <algorithm>
@@ -29,32 +33,57 @@
 template <typename T>
 struct atomwrapper
 {
-    std::atomic<T> _a;
+    std::atomic<T> value;
 
     atomwrapper()
-      : _a()
+      : value()
     {
     }
 
     atomwrapper(const std::atomic<T>& a)
-      : _a(a.load())
+      : value(a.load())
     {
     }
 
     atomwrapper(const atomwrapper& other)
-      : _a(other._a.load())
+      : value(other.value.load())
     {
     }
 
     atomwrapper& operator=(const atomwrapper& other)
     {
-        _a.store(other._a.load());
+        value.store(other.value.load());
     }
 };
 
 using gc_type = cds::gc::custom_HP<cds::gc::hp::details::HPXTLSManager>;
 using key_type = std::size_t;
 using value_type = atomwrapper<std::size_t>;
+
+// Declare michael_list traits
+struct michael_list_trait : public cds::container::michael_list::traits
+{
+    typedef std::less<std::size_t> less;
+};
+// Declare traits-based list
+using michaelkvlist = cds::container::MichaelKVList<gc_type, key_type,
+    value_type, michael_list_trait>;
+
+// Declare split_list_traits
+struct split_list_traits : public cds::container::split_list::traits
+{
+    // what type of ordered list we want to use
+    typedef cds::container::michael_list_tag ordered_list;
+    // hash functor for the key stored in split-list map
+    typedef std::hash<key_type> hash;
+
+    // Type traits for our MichaelList class
+    struct ordered_list_traits : public cds::container::michael_list::traits
+    {
+        // use our std::less predicate as comparator to order list nodes
+        typedef std::less<key_type> less;
+    };
+};
 
 template <typename Map>
 void run(Map& map, const std::size_t n_items, const std::size_t n_threads)
@@ -82,17 +111,16 @@ void run(Map& map, const std::size_t n_items, const std::size_t n_threads)
     auto insert_val_and_increase_counter =
         [&](std::vector<std::size_t>& number_vec) {
             hpx::cds::hpxthread_manager_wrapper cds_hpx_wrap;
-            hpx::this_thread::sleep_for(std::chrono::seconds(rand() % 5));
             for (auto val : number_vec)
             {
+                if (rand() % 10 == 0)
+                    hpx::this_thread::yield();
                 typename Map::guarded_ptr gp;
 
                 gp = map.get(val);
-                if (gp)
-                {
-                    (gp->second)._a++;
-                    counter_vec[gp->first]++;
-                }
+                HPX_ASSERT(gp);
+                (gp->second).value++;
+                counter_vec[gp->first]++;
             }
         };
 
@@ -113,32 +141,44 @@ void run(Map& map, const std::size_t n_items, const std::size_t n_threads)
 
     for (auto it = map.cbegin(); it != map.cend(); ++it)
     {
-        HPX_TEST_EQ(counter_vec[it->first], (it->second)._a);
-        //        std::cout << "map key: " << it->first << " value: " << (it->second)._a
-        //                  << " and counter_vec: " << counter_vec[it->first] << "\n";
+        HPX_TEST_EQ(counter_vec[it->first], (it->second).value);
     }
 }
 
 int hpx_main(int, char**)
 {
-    using map_type =
+    using feldmanhash_map_type =
         cds::container::FeldmanHashMap<gc_type, key_type, value_type>;
+    using michaelhash_map_type =
+        cds::container::MichaelHashMap<gc_type, michaelkvlist>;
+    using splitlist_map_type = cds::container::SplitListMap<gc_type, key_type,
+        value_type, split_list_traits>;
+
+    const std::size_t max_hazard_pointer =
+        (std::max)({feldmanhash_map_type::c_nHazardPtrCount + 1,
+            michaelhash_map_type::c_nHazardPtrCount + 1,
+            splitlist_map_type::c_nHazardPtrCount + 1});
 
     // Initialize libcds and hazard pointer
     hpx::cds::libcds_wrapper cds_init_wrapper(
-        hpx::cds::smr_t::hazard_pointer_hpxthread,
-        map_type::c_nHazardPtrCount + 1, 100, 16);
+        hpx::cds::smr_t::hazard_pointer_hpxthread, max_hazard_pointer, 100, 16);
 
     {
         // enable this thread/task to run using libcds support
         hpx::cds::hpxthread_manager_wrapper cds_hpx_wrap;
 
-        map_type map;
-
         const std::size_t n_items = 10000;
-        const std::size_t n_threads = 10;
+        const std::size_t n_threads = 100;
+        // load factor: estimation of max number of items in the bucket
+        const std::size_t n_load_factor = 100;
 
-        run(map, n_items, n_threads);
+        feldmanhash_map_type feldmanhash_map;
+        michaelhash_map_type michaelhash_map(n_items, n_load_factor);
+        splitlist_map_type splitlist_map(n_items, n_load_factor);
+
+        run(feldmanhash_map, n_items, n_threads);
+        run(michaelhash_map, n_items, n_threads);
+        run(splitlist_map, n_items, n_threads);
     }
 
     return hpx::finalize();
