95a07ea8a50ba6befb44df5299b5937311ecb3f9
Jiakun Yan <jiakuny3@illinois.com>
2021-12-16 16:50:06 -0600

    WIP: add cmake fetchcontent support for LCI
    don't work with HPX_WITH_PKGCONFIG=ON. Have something to do with $<COMPILE_LANGUAGE:CXX>

--- cmake/HPX_SetupLCI.cmake
+++ cmake/HPX_SetupLCI.cmake
@@ -8,8 +8,66 @@
 # corresponding plugin
 
 macro(setup_lci)
-  if(NOT TARGET LCI::Shared)
-    find_package(LCI REQUIRED)
+  if(NOT TARGET LCI::LCI)
+    if(NOT HPX_WITH_FETCH_LCI)
+      find_package(LCI REQUIRED)
+    elseif(NOT HPX_FIND_PACKAGE)
+      if(FETCHCONTENT_SOURCE_DIR_LCI)
+        hpx_info(
+                "HPX_WITH_FETCH_LCI=${HPX_WITH_FETCH_LCI}, LCI will be used through CMake's FetchContent and installed alongside HPX (FETCHCONTENT_SOURCE_DIR_LCI=${FETCHCONTENT_SOURCE_DIR_LCI})"
+        )
+      else()
+        hpx_info(
+                "HPX_WITH_FETCH_LCI=${HPX_WITH_FETCH_LCI}, Asio will be fetched using CMake's FetchContent and installed alongside HPX (HPX_WITH_LCI_TAG=${HPX_WITH_LCI_TAG})"
+        )
+      endif()
+      include(FetchContent)
+      fetchcontent_declare(
+              lci
+              GIT_REPOSITORY https://github.com/uiuc-hpc/LC.git
+              GIT_TAG ${HPX_WITH_LCI_TAG}
+      )
+
+      fetchcontent_getproperties(lci)
+      if(NOT lci_POPULATED)
+        fetchcontent_populate(lci)
+        set(LCI_WITH_EXAMPLES OFF CACHE INTERNAL "")
+        set(LCI_WITH_TESTS OFF CACHE INTERNAL "")
+        set(LCI_WITH_BENCHMARKS OFF CACHE INTERNAL "")
+        set(LCI_WITH_DOC OFF CACHE INTERNAL "")
+        set(LCI_SERVER ibv CACHE INTERNAL "")
+        set(LCI_PM_BACKEND mpi CACHE INTERNAL "")
+        add_subdirectory(${lci_SOURCE_DIR} ${lci_BINARY_DIR})
+        add_library(LCI::LCI ALIAS LCI)
+      endif()
+
+      install(
+              TARGETS LCI
+              EXPORT HPXLCITarget
+              COMPONENT core
+      )
+
+      install(
+              DIRECTORY ${lci_SOURCE_DIR}/include/
+              DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+              COMPONENT core
+              FILES_MATCHING
+              PATTERN "*.h"
+      )
+
+      export(
+              TARGETS LCI
+              NAMESPACE LCI::
+              FILE "${CMAKE_CURRENT_BINARY_DIR}/lib/cmake/${HPX_PACKAGE_NAME}/HPXLCITarget.cmake"
+      )
+
+      install(
+              EXPORT HPXLCITarget
+              NAMESPACE LCI::
+              FILE HPXLCITarget.cmake
+              DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${HPX_PACKAGE_NAME}
+      )
+    endif()
   endif()
 endmacro()
 
